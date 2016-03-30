4b2665fc2c9e11486cf69671e78ec38af21f92b0
Jo Ann Brooks <joannbrooks036@gmail.com>
2016-03-30 16:20:57 -0500

    basic animal OOP exercise trial

---
+++ src/animal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace animal
+{
+    public class Animal
+    {
+        public string name;
+    }
+
+    public class AnimalAttributes
+    {
+        static void Main()
+        {
+            Animal a1 = new Animal();
+            a1.name = "Aardvark";
+
+            Animal a2 = new Animal();
+            a2.name = "Zebra";
+
+            Console.WriteLine(a1.name);
+            Console.WriteLine(a2.name);
+        }
+    }
+}
